51dac71a63eb865c6be6e4ecb6671a1d0d8646c5
KOTHAPALLIRAJASEKHARREDDY <rajasekharreddy752@gmail.com>
2025-04-12 20:37:34 -0500

    Create Secure Chat Project

---
+++ src/main/java/org/project/CreateRoomServlet.java
@@ -0,0 +1,15 @@
+package org.project;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CreateRoomServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
