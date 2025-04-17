54443c71176115a927ad2b5a0859a99b7b1e9361
KOTHAPALLIRAJASEKHARREDDY <rajasekharreddy752@gmail.com>
2025-04-17 12:37:15 -0500

    Added chatutil

---
+++ src/main/java/org/project/ChatUtil.java
@@ -0,0 +1,47 @@
+package org.project;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class ChatUtil {
+    private static Hashtable<String, ArrayList<String>> rooms = new Hashtable<>();
+    public static Hashtable<String, ArrayList<String>> getRooms() {
+        return rooms;
+    }
+
+    public static ArrayList<String> getUserFromRoomId(String roomId)
+    {
+        return rooms.get(roomId);
+    }
+
+    public static void setUserFromRoomId(String roomId, String userId)
+    {
+        if (rooms.containsKey(roomId))
+        {
+            rooms.get(roomId).add(userId);
+        }
+        else
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(userId);
+            rooms.put(roomId, list);
+        }
+    }
+    public static boolean delUserFromRoomId(String roomId, String userId)
+    {
+        if (rooms.containsKey(roomId))
+        {
+            return rooms.get(roomId).remove(userId);
+        }
+        else
+        {
+            System.out.println("Room not found");
+            return false;
+        }
+    }
+
+    public static ArrayList<String> delRoom(String roomId)
+    {
+        return rooms.remove(roomId);
+    }
+}
